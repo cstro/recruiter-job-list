f47997c879f677cdb03405b1c39b2e538ba31a56
Chris Stroud <chris.stroud00@gmail.com>
2021-06-24 21:47:26 +0100

    Add eslint linting

--- src/App.js
+++ src/App.js
@@ -1,58 +1,59 @@
-import {useState} from 'react'
-import JobListItem from './components/JobListItem'
-import matchedJobs from './data/matched_jobs.json'
+import React, { useState } from "react"
+import JobListItem from "./components/JobListItem"
+import matchedJobs from "./data/matched_jobs.json"
 
 const { recruiters } = matchedJobs
 
-const jobs = Object.entries(matchedJobs.jobs).map(([key, value]) => (
-  {
-    ...value,
-    key
-  }
-))
+const jobs = Object.entries(matchedJobs.jobs).map(([key, value]) => ({
+  ...value,
+  key,
+}))
 
 function App() {
-  const [filter, setFilter] = useState('all')
+  const [filter, setFilter] = useState("all")
 
   function getRecruiter(id) {
     return recruiters[id]
   }
 
   // TODO: Is there a better way to do this reactively?
-  function filteredJobs () {
-    if (filter === 'all') {
+  function filteredJobs() {
+    if (filter === "all") {
       return jobs
     }
 
-    return jobs.filter((job) => parseInt(filter) === job.placed_by)
+    return jobs.filter((job) => parseInt(filter, 10) === job.placed_by)
   }
 
   return (
     <div className="flex m-8">
-      <div class="bg-grey-100 p-8">
-      <select selected={filter} onChange={(event) => setFilter(event.target.value)}>
-        <option value={'all'}>Show all</option>
-        {Object.entries(recruiters).map(([id, recruiter]) => <option value={id}>{recruiter.name}</option>)}
-      </select>
+      <div className="bg-grey-100 p-8">
+        <select
+          selected={filter}
+          onBlur={(event) => setFilter(event.target.value)}
+        >
+          <option value="all">Show all</option>
+          {Object.entries(recruiters).map(([id, recruiter]) => (
+            <option value={id}>{recruiter.name}</option>
+          ))}
+        </select>
 
-      <div>Recruiter rating</div>
-      <div>Salary</div>
-      <div>Location</div>
+        <div>Recruiter rating</div>
+        <div>Salary</div>
+        <div>Location</div>
       </div>
 
-      <div class="flex-1">
-
-      Search results: {filteredJobs().length}
-
-      <ul>
-        {filteredJobs().map((job) => {
-          const recruiter = getRecruiter(job.placed_by)
-          return <JobListItem key={job.key} job={job} recruiter={recruiter} />
-        })}
-      </ul>
+      <div className="flex-1">
+        Search results: {filteredJobs().length}
+        <ul>
+          {filteredJobs().map((job) => {
+            const recruiter = getRecruiter(job.placed_by)
+            return <JobListItem key={job.key} job={job} recruiter={recruiter} />
+          })}
+        </ul>
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
