c11dd4b51c49d29ad231c3d08452835d5b87b6d5
Chris Stroud <chris.stroud00@gmail.com>
2021-06-27 17:16:46 +0100

    Add max width and center with margin

--- src/App.js
+++ src/App.js
@@ -17,16 +17,18 @@ function App() {
   }, [])
 
   return (
-    <div className="flex p-8 bg-gray-50 min-h-screen">
-      <SearchFilters />
-      <div className="flex-1">
-        <div className="mb-4">
-          <span className="font-bold">
-            Search results:{" "}
-            <span data-test="search-result-count">{jobs.length}</span>
-          </span>
+    <div className="p-8 bg-gray-50 min-h-screen">
+      <div className="flex max-w-4xl m-auto">
+        <SearchFilters />
+        <div className="flex-1">
+          <div className="mb-4">
+            <span className="font-bold">
+              Search results:{" "}
+              <span data-test="search-result-count">{jobs.length}</span>
+            </span>
+          </div>
+          <JobList />
         </div>
-        <JobList />
       </div>
     </div>
   )
