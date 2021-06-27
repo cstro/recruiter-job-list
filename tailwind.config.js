58e79dc77dd11372270e169bcba52d54b06854b6
Chris Stroud <chris.stroud00@gmail.com>
2021-06-27 17:45:46 +0100

    Add border, bg and text colors to safelist
    This stops PurgeCSS from removing the styles in production even though
    it can't find them. This is needed because we use a computed value for
    the text, bg border color inside the JobStatusTag component.
    
    See more about tailwind safelist here:
    https://tailwindcss.com/docs/optimizing-for-production#safelisting-specific-classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,23 @@
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  purge: {
+    content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+    options: {
+      safelist: [
+        "text-green-500",
+        "bg-green-100",
+        "border-green-200",
+        "text-gray-500",
+        "bg-gray-100",
+        "border-gray-200",
+        "text-pink-500",
+        "bg-pink-100",
+        "border-pink-200",
+        "text-yellow-500",
+        "bg-yellow-100",
+        "border-yellow-200",
+      ],
+    },
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
